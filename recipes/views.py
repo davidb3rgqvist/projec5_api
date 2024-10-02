565c06728bcd852006be76024d4822574dc237d6
David Bergqvist <David.bergqvist@gmail.com>
2024-10-02 18:44:25 +0000

    added is user or read only to the recipe, added the urls

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets, permissions
 from .models import Recipe, Comment
 from .serializers import RecipeSerializer, CommentSerializer
+from project5_api.permissions import IsOwnerOrReadOnly 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all().order_by('-created_at')
     serializer_class = RecipeSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -14,7 +15,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
