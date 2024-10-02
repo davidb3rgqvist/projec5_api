d19dd890d9d328479a0c85379fefd7f47c6732c1
David Bergqvist <David.bergqvist@gmail.com>
2024-10-02 19:32:28 +0000

    updated the comments serializers

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Recipe, Comment
+from profiles.models import Profile
 
 class RecipeSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -19,7 +20,14 @@ class RecipeSerializer(serializers.ModelSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     class Meta:
         model = Comment
-        fields = ['id', 'recipe', 'owner', 'content', 'created_at', 'updated_at']
+        fields = ['id', 'recipe', 'owner', 'content', 'created_at', 'updated_at', 'is_owner', 'profile_id', 'profile_image']
+
+    def get_is_owner(self, obj):
+        request = self.context.get('request')
+        return request.user == obj.owner
