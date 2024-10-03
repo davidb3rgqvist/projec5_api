71bf8782e5acc07f13f9913435acce5a07475622
David Bergqvist <David.bergqvist@gmail.com>
2024-10-03 08:22:47 +0000

    added allauth dependency

--- project5_api/urls.py
+++ project5_api/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('', include('recipes.urls')),
     path('', include('likes.urls')),
     path('', include('profiles.urls')),
