631bba0cc18de93da8275a2d092e5c64f7fe9c74
David Bergqvist <David.bergqvist@gmail.com>
2024-10-16 18:25:38 +0000

    updated profiles urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
          name='profile-list'),
      path('profiles/<int:pk>/', views.ProfileDetail.as_view(), 
          name='profile-detail'),
-     path('profiles/<int:pk>/', views.ProfileDelete.as_view(), 
+     path('profiles/<int:pk>/delete/', views.ProfileDelete.as_view(), 
           name='profile-delete'),
 ]
