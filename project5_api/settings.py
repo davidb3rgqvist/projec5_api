e8d05a231d727839a33c10e5597fb7d4a6271edf
David Bergqvist <David.bergqvist@gmail.com>
2024-11-29 21:35:14 +0000

    updated setting for deployment

--- project5_api/settings.py
+++ project5_api/settings.py
@@ -42,9 +42,9 @@ REST_FRAMEWORK = {
 
 # CSRF trusted origins
 CSRF_TRUSTED_ORIGINS = [
-  "https://foorky-fe-79ffc00345fc.herokuapp.com",
   "https://3000-davidb3rgqv-project5foo-75up2jzyvte.ws.codeinstitute-ide.net",
-  "https://8000-davidb3rgqv-project5api-dff603ub8x0.ws.codeinstitute-ide.net",
+  "https://foorky-fe-79ffc00345fc.herokuapp.com",
+  "https://project5-api-a299de19cbb3.herokuapp.com"
 ]
 
 # JWT authentication settings
@@ -77,13 +77,10 @@ DEBUG = 'DEV' in os.environ
 
 # Allowed hosts
 ALLOWED_HOSTS = [
-    '8000-davidb3rgqv-project5api-dff603ub8x0.ws.codeinstitute-ide.net',
-    '127.0.0.1:8000',
-    '127.0.0.1',
-    '3000-davidb3rgqv-project5foo-75up2jzyvte.ws.codeinstitute-ide.net',
-    'foorky-fe-79ffc00345fc.herokuapp.com',
     'project5-api-a299de19cbb3.herokuapp.com',
-    'localhost'
+    'foorky-fe-79ffc00345fc.herokuapp.com',
+    '127.0.0.1',
+    'localhost',
 ]
 
 # Application definition
