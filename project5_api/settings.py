7197ad439baa926015fd789a20d359aeff8c0feb
David Bergqvist <David.bergqvist@gmail.com>
2024-11-29 15:40:07 +0000

    added heroku fronten to CSRF trusted origins

--- project5_api/settings.py
+++ project5_api/settings.py
@@ -42,6 +42,7 @@ REST_FRAMEWORK = {
 
 # CSRF trusted origins
 CSRF_TRUSTED_ORIGINS = [
+  "https://foorky-fe-79ffc00345fc.herokuapp.com"
   "https://3000-davidb3rgqv-project5foo-75up2jzyvte.ws.codeinstitute-ide.net",
   "https://8000-davidb3rgqv-project5api-dff603ub8x0.ws.codeinstitute-ide.net",
 ]
